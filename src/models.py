789120b12f4984227f247d186d9012540dcbec2c
Emilioj17 <ejjelves@gmail.com>
2021-06-25 19:11:15 +0000

    Creo que Listo

--- src/models.py
+++ src/models.py
@@ -8,26 +8,95 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(50), nullable=False)
+    apellido = Column(String(50), nullable=False)
+    correo = Column(String(50), nullable=False)
+    clave = Column(String(50), ForeignKey('claves.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personaje(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
 
     def to_dict(self):
-        return {}
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
+
+class Planeta(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
+
+class Clave(Base):
+    __tablename__ = 'claves'
+    id = Column(Integer, primary_key=True)
+    clave = Column(String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+            "clave": self.clave
+        }
+
+class Nave(Base):
+    __tablename__ = 'naves'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
+
+class Pelicula(Base):
+    __tablename__ = 'peliculas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
+
+class Especie(Base):
+    __tablename__ = 'especies'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
+
+class Vehiculo(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre":self.name
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
